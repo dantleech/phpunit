8e49ddae7c331b2390f70bf5655286579e6def74
Daniel Leech <daniel@dantleech.com>
2025-02-10 23:06:36 +0000

    CS

--- src/TextUI/Configuration/SourceFilter.php
+++ src/TextUI/Configuration/SourceFilter.php
@@ -9,6 +9,10 @@
  */
 namespace PHPUnit\TextUI\Configuration;
 
+use function preg_match;
+use function sprintf;
+use Webmozart\Glob\Glob;
+
 /**
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
  *
@@ -18,34 +22,79 @@ final class SourceFilter
 {
     private static ?self $instance = null;
 
-    /**
-     * @var array<non-empty-string, true>
-     */
-    private readonly array $map;
-
     public static function instance(): self
     {
         if (self::$instance === null) {
             self::$instance = new self(
-                (new SourceMapper)->map(
-                    Registry::get()->source(),
-                ),
+                Registry::get()->source(),
             );
         }
 
         return self::$instance;
     }
 
-    /**
-     * @param array<non-empty-string, true> $map
-     */
-    public function __construct(array $map)
+    public function __construct(private Source $source)
     {
-        $this->map = $map;
     }
 
     public function includes(string $path): bool
     {
-        return isset($this->map[$path]);
+        $includedFile = false;
+
+        foreach ($this->source->includeFiles() as $file) {
+            if ($file->path() === $path) {
+                $includedFile = true;
+            }
+        }
+        $includePatterns = $this->toGlobs($this->source->includeDirectories());
+        $excludePatterns = $this->toGlobs($this->source->excludeDirectories());
+
+        if ($includedFile === false) {
+            foreach ($includePatterns as $pattern) {
+                if (Glob::match($path, $pattern)) {
+                    return false;
+                }
+            }
+        }
+
+        foreach ($this->source->excludeFiles() as $file) {
+            if ($file->path() === $path) {
+                return false;
+            }
+        }
+
+        foreach ($excludePatterns as $pattern) {
+            if (Glob::match($path, $pattern)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function toGlobs(FilterDirectoryCollection $dirs): array
+    {
+        $globs = [];
+
+        foreach ($dirs as $dir) {
+            $globs[] = sprintf('%s%s/*%s', $dir->path(), $dir->prefix(), $dir->suffix());
+        }
+
+        return $globs;
+    }
+
+    private function matches(array $includePatterns, string $path): bool
+    {
+        $matched = false;
+
+        foreach ($includePatterns as $includePattern) {
+            if (preg_match('~^/home/daniel/www/phpunit/phpunit/[^/]*[^/]*\.php$~', $path)) {
+                $matched = true;
+
+                break;
+            }
+        }
+
+        return $matched;
     }
 }
